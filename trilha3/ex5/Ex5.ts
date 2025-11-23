527df20eafb4a1774eed619053b12c95741eb1da
marchesido <marchesido@gmail.com>
2025-11-22 23:13:55 -0300

    Ex5classesExtends

--- trilha3/ex5/Ex5.ts
+++ trilha3/ex5/Ex5.ts
@@ -18,3 +18,28 @@ abstract class Funcionario {
     abstract calcularBonus(): number;
 }
 
+class Gerente extends Funcionario {
+    calcularBonus(): number {
+        return this.getSalario() * 0.10; // 10%
+    }
+}
+
+class Operario extends Funcionario {
+    calcularBonus(): number {
+        return this.getSalario() * 0.05; // 5%
+    }
+}
+
+function calcularSalarioComBonus(funcionarios: Funcionario[]): void {           // Função que usa polimorfismo
+    funcionarios.forEach(func => {
+        const bonus = func.calcularBonus();
+        const salarioFinal = func.getSalario() + bonus;
+
+        console.log(
+            `${func.getNome()} — Salário base: R$ ${func.getSalario().toFixed(2)}, ` +
+            `Bônus: R$ ${bonus.toFixed(2)}, ` +
+            `Salário final: R$ ${salarioFinal.toFixed(2)}`
+        );
+    });
+}
+
