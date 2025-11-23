042a7eb2845d05bdf160dfaf0056a5132fb90265
marchesido <marchesido@gmail.com>
2025-11-22 23:13:07 -0300

    Ex5ClassePai

---
+++ trilha3/ex5/Ex5.ts
@@ -0,0 +1,20 @@
+abstract class Funcionario {
+    private nome: string;
+    private salario: number;
+
+    constructor(nome: string, salario: number) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    getNome(): string {
+        return this.nome;
+    }
+
+    getSalario(): number {
+        return this.salario;
+    }
+
+    abstract calcularBonus(): number;
+}
+
